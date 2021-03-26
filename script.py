7066b4b42eb1ca85f64e1ba7168910d68a28a89a
theSekyi <emmanuelkwamesekyi@gmail.com>
2021-03-26 12:14:05 +0200

    initial workflow

---
+++ script.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.datasets import load_iris
+
+
+
+
+
+
+data = load_iris()
+
+
+
+
+def data_stats(data):
+
+
+    labels = data.target_names
+    df = pd.DataFrame(data, columns = labels)
+    
+    return df.describe()
+
+
+
+print(data_stats(data))
