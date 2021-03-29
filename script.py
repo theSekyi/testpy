b96dc0b4e4d930d90eb4eed1f83d2d2515bff516
theSekyi <emmanuelkwamesekyi@gmail.com>
2021-03-29 11:47:46 +0200

    added formatter

--- script.py
+++ script.py
@@ -1,13 +1,32 @@
 import pandas as pd
+
+
 from sklearn.datasets import load_iris
 
 
 data = load_iris()
 
 
+
+
+
+
+
+
+
+
+
+
 def data_stats(data):
     labels = data.target_names
     df = pd.DataFrame(data, columns=labels)
+
+
+
+
+
+
+
 
     return df
 
