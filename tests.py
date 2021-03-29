0482f26d90ce732e0698da9cbf01aa0960de4e66
theSekyi <emmanuelkwamesekyi@gmail.com>
2021-03-29 11:00:36 +0200

    added formatter

--- tests.py
+++ tests.py
@@ -4,6 +4,6 @@ from sklearn.datasets import load_iris
 
 data = load_iris()
 
-# def test_columns():
-#     df =data_stats(data)
-#     assert len(df.columns) ==
+def test_columns():
+    df =data_stats(data)
+    assert len(df.columns) == 6
