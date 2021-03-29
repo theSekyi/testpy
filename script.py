07964c53fe0861d15cad8a28591e06e938231602
Emmanuel Sekyi <emmanuel-sekyi@users.noreply.github.com>
2021-03-29 09:54:29 +0000

    Automated changes

--- script.py
+++ script.py
@@ -5,26 +5,11 @@ from sklearn.datasets import load_iris
 data = load_iris()
 
 
-
-
 def data_stats(data):
     labels = data.target_names
     df = pd.DataFrame(data, columns=labels)
 
-
-
-
     return df
-
-
-
-
-
-
-
-
-
-
 
 
 print(data_stats(data))
