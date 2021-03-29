1bdcb1d8f448da4dc00aef148c68ad0c50cea277
Emmanuel <emmanuel-sekyi@users.noreply.github.com>
2021-03-29 10:13:42 +0000

    Automated changes

--- script.py
+++ script.py
@@ -9,22 +9,12 @@ def load_data(data):
     labels = data.target_names
     df = pd.DataFrame(data, columns=labels)
 
-
-
-
     return df
 
 
 def preprocess(df):
 
-
-
-
     pass
-
-
-
-
 
 
 print(load_data(data))
